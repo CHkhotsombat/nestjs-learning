47b4611f974a54a95171c78cc9f30700e4a2ccd1
Chaiyasit.kh <chaiyasit.kh.57@gmail.com>
2023-06-19 20:36:05 +0700

    :pencil2: Add nestjs-i18n

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,25 @@ import { PrismaModule } from './prisma/prisma.module';
 import { ArticlesModule } from './articles/articles.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import {
+  HeaderResolver,
+  I18nModule,
+  QueryResolver,
+  AcceptLanguageResolver,
+} from 'nestjs-i18n';
+import * as path from 'path';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    I18nModule.forRoot({
+      fallbackLanguage: 'en',
+      loaderOptions: {
+        path: 'src/i18n/',
+        watch: true,
+      },
+      resolvers: [new HeaderResolver(['x-lang']), AcceptLanguageResolver],
+    }),
     PrismaModule,
     ArticlesModule,
     UsersModule,
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { I18nContext } from 'nestjs-i18n';
 
 @Injectable()
 export class AppService {
-  getHello() {
-    return { service: 'This is NestJs Learning APIs services.' };
+  getHello(i18n: I18nContext) {
+    return {
+      service: i18n.t('general.service'),
+      welcome: i18n.t('general.welcome'),
+      language: i18n.lang,
+    };
   }
 }

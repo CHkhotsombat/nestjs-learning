4c5d68093e0cac9156fb59a95164a1a426fb51bc
Chaiyasit.kh <chaiyasit.kh.57@gmail.com>
2023-06-19 12:46:52 +0700

    🎨format code

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,7 +8,13 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, ArticlesModule, UsersModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+    ArticlesModule,
+    UsersModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
